6af89875d99d7ce9db391a5b8fa1c75384a8da7a
Md Mehedi Hasan Jony <109722698+MhJony-066@users.noreply.github.com>
2022-08-10 08:42:45 +0600

    Add files via upload

---
+++ LAB 2/066-49_10_5.cpp
@@ -0,0 +1,13 @@
+//Codefoces Problem NO :50A
+//Problem Name :Domino piling
+
+#include<iostream>
+using namespace std;
+int main()
+{
+ int x,y,z;
+ cout<<"enter your board :";
+ cin>>x>>y;
+ z=(x*y)/2;
+ cout<<z<<endl;
+}
